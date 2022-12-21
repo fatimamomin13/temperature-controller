5985491443323adb9b403691b31dd4702b8c98bb
fatimamomin13 <fatimamomin2812@gmail.com>
2022-12-21 23:20:42 +0530

    Temperature Controller Implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Snowfall from "react-snowfall";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(10);
+  const [color, setColor] = useState("cold");
+  const [snow, setSnow] = useState(<Snowfall />);
+  const [bgcolor, setBgcolor] = useState("rgb(32, 31, 31)");
+  // (document.body.style = "background: rgb(32, 31, 31);")
+
+  document.body.style = `background: ${bgcolor} `;
+  const increment = () => {
+    let newCount = count + 1;
+    setCount(newCount);
+    if (newCount >= 15) {
+      setColor("hot");
+      setSnow("");
+      setBgcolor("rgb(255, 255, 125)");
+    }
+  };
+
+  const decrement = () => {
+    let newCount = count - 1;
+    setCount(newCount);
+    if (newCount < 15) {
+      setColor("cold");
+      setSnow(<Snowfall />);
+      setBgcolor("rgb(32, 31, 31)");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <div className="container">
+          <div className={`display ${color}`}>
+            <h1>{count}°C</h1>
+          </div>
+          <div className="button-div">
+            <button onClick={increment} className="button">
+              +
+            </button>
+            <button onClick={decrement} className="button">
+              -
+            </button>
+          </div>
+        </div>
+      </div>
+      {snow}
+    </>
   );
 }
 
